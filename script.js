d7e4b9dcb42310d13ed5ed3e6425698a9fb38726
cpaschall <cpaschall216@gmail.com>
2022-03-27 22:02:38 -0400

    changing question functions in js file

--- script.js
+++ script.js
@@ -62,137 +62,140 @@
 
 // 1. Create all pages in javascript then append to html file.  Each question/start pg/high-score has to take up the same space
 //     - possible I can create each question in html, all under a single div so it takes up the same space.  Then give each question an id and switch display for each id to "none" then use javascript to change the display attribut so only that question shows when it's time.
-var buttonEl = document.querySelector(".answerBtn");
-var displayEl = document.querySelector(".current-display");
-var getSection = document.querySelectorAll("section");
-var answerConfirm = document.createElement("p");
-var createBtnAnswer = document.createElement("button");
-var createBtnWrong1 = document.createElement("button");
-var createBtnWrong2 = document.createElement("button");
-var createBtnWrong3 = document.createElement("button");
-var allBtns = document.querySelectorAll("button");
-var createH2 = document.createElement("h2");
-var startPage = document.querySelector("#start-page")
-var startQuiz = document.querySelector("#start-quiz")
-var answerBtn = document.querySelector("#answer-btn");
-var wrongBtn = document.querySelector(".wrong-btn");
-createBtnAnswer.setAttribute("id", "answer-btn");
-createBtnWrong1.setAttribute("class", "wrong-btn");
-createBtnWrong2.setAttribute("class", "wrong-btn");
-createBtnWrong3.setAttribute("class", "wrong-btn");
+// var buttonEl = document.querySelector(".answerBtn");
+// var displayEl = document.querySelector(".current-display");
+// var getSection = document.querySelectorAll("section");
+// var answerConfirm = document.createElement("p");
+// var createBtnAnswer = document.createElement("button");
+// var createBtnWrong1 = document.createElement("button");
+// var createBtnWrong2 = document.createElement("button");
+// var createBtnWrong3 = document.createElement("button");
+// createBtnAnswer.setAttribute("id", "answer-btn");
+// createBtnWrong1.setAttribute("class", "wrong-btn");
+// createBtnWrong2.setAttribute("class", "wrong-btn");
+// createBtnWrong3.setAttribute("class", "wrong-btn");
+// var allBtns = document.querySelectorAll("button");
+// var createH2 = document.createElement("h2");
+// var startPage = document.querySelector("#start-page")
+// var startQuiz = document.querySelector("#start-quiz")
+// var answerBtn = document.querySelector("#answer-btn");
+// var wrongBtn = document.querySelector(".wrong-btn");
+
 
 // var answerBtn = document.querySelector("#answer-btn");
 // var wrongBtn = document.querySelector(".wrong-btn");
 
 
-var q1 = {
-    question: "Test Question1",
-    wrongAns1 : "wrong 1",
-    wrongAns2 : "wrong 2",
-    wrongAns3 : "wrong 3",
-    rightAns : "right",
-}; 
-var q2 = {
-    question: "Test Question2",
-    wrongAns1 : "wrong 1",
-    wrongAns2 : "wrong 2",
-    wrongAns3 : "wrong 3",
-    rightAns : "right",
-}; 
-var q3 = {
-    question: "Test Question3",
-    wrongAns1 : "wrong 1",
-    wrongAns2 : "wrong 2",
-    wrongAns3 : "wrong 3",
-    rightAns : "right",
-}; 
-var q4 = {
-    question: "Test Question4",
-    wrongAns1 : "wrong 1",
-    wrongAns2 : "wrong 2",
-    wrongAns3 : "wrong 3",
-    rightAns : "right",
-}; 
-var q5 = {
-    question: "Test Question5",
-    wrongAns1 : "wrong 1",
-    wrongAns2 : "wrong 2",
-    wrongAns3 : "wrong 3",
-    rightAns : "right",
-}; 
+// var q1 = {
+//     question: "Test Question1",
+//     wrongAns1 : "wrong 1",
+//     wrongAns2 : "wrong 2",
+//     wrongAns3 : "wrong 3",
+//     rightAns : "right",
+// }; 
+// var q2 = {
+//     question: "Test Question2",
+//     wrongAns1 : "wrong 1",
+//     wrongAns2 : "wrong 2",
+//     wrongAns3 : "wrong 3",
+//     rightAns : "right",
+// }; 
+// var q3 = {
+//     question: "Test Question3",
+//     wrongAns1 : "wrong 1",
+//     wrongAns2 : "wrong 2",
+//     wrongAns3 : "wrong 3",
+//     rightAns : "right",
+// }; 
+// var q4 = {
+//     question: "Test Question4",
+//     wrongAns1 : "wrong 1",
+//     wrongAns2 : "wrong 2",
+//     wrongAns3 : "wrong 3",
+//     rightAns : "right",
+// }; 
+// var q5 = {
+//     question: "Test Question5",
+//     wrongAns1 : "wrong 1",
+//     wrongAns2 : "wrong 2",
+//     wrongAns3 : "wrong 3",
+//     rightAns : "right",
+// }; 
 
 // var questions = [q1, q2, q3, q4, q5];
 
 
 
 
-function chooseAnswer() {
+// function chooseAnswer() {
     // var answerBtn = document.querySelector("#answer-btn");
     // var wrongBtn = document.querySelector(".wrong-btn");
 
-    answerBtn.addEventListener("click", function() { 
-        answerConfirm.textContent = "Correct Answer";
-        getSection[1].appendChild(answerConfirm);
-        return
-    });
-    wrongBtn.addEventListener("click", function() { 
-        answerConfirm.textContent = "Wrong Answer";
-        getSection[1].appendChild(answerConfirm);
-        return
-    });
-}
-function displayFirstQuestion() {
-    createH2.textContent = q1.question;
-    getSection[1].appendChild(createH2);
-    createBtnWrong1.textContent = q1.wrongAns1;
-    getSection[1].appendChild(createBtnWrong1);
-    createBtnWrong2.textContent = q1.wrongAns2;
-    getSection[1].appendChild(createBtnWrong2);
-    createBtnWrong3.textContent = q1.wrongAns2;
-    getSection[1].appendChild(createBtnWrong3);
-    createBtnAnswer.textContent = q1.rightAns;
-    getSection[1].appendChild(createBtnAnswer);
-    chooseAnswer();
+//     answerBtn.addEventListener("click", function() { 
+//         answerConfirm.textContent = "Correct Answer";
+//         getSection[1].appendChild(answerConfirm);
+//         return
+//     });
+//     wrongBtn.addEventListener("click", function() { 
+//         answerConfirm.textContent = "Wrong Answer";
+//         getSection[1].appendChild(answerConfirm);
+//         return
+//     });
+// }
+// function displayFirstQuestion() {
+//     createH2.textContent = q1.question;
+//     getSection[1].appendChild(createH2);
+//     createBtnWrong1.textContent = q1.wrongAns1;
+//     getSection[1].appendChild(createBtnWrong1);
+//     createBtnWrong2.textContent = q1.wrongAns2;
+//     getSection[1].appendChild(createBtnWrong2);
+//     createBtnWrong3.textContent = q1.wrongAns2;
+//     getSection[1].appendChild(createBtnWrong3);
+//     createBtnAnswer.textContent = q1.rightAns;
+//     getSection[1].appendChild(createBtnAnswer);
+//     answerBtn.addEventListener("click", function() {
+//         console.log("it worked!");
+//     });
    
-}
+// }
 
 
-function displayQuestion() {
-    var questions = [q1, q2, q3, q4, q5];
-    for(var i = 1; i < questions.length; i++){
-        // chooseAnswer()
-        createH2.textContent = questions[i].question;
-        getSection[1].appendChild(createH2);
-        createBtnWrong1.textContent = questions[i].wrongAns1;
-        getSection[1].appendChild(createBtnWrong1);
-        createBtnWrong2.textContent = questions[i].wrongAns2;
-        getSection[1].appendChild(createBtnWrong2);
-        createBtnWrong3.textContent = questions[i].wrongAns2;
-        getSection[1].appendChild(createBtnWrong3);
-        createBtnAnswer.textContent = questions[i].rightAns;
-        getSection[1].appendChild(createBtnAnswer);
-        // chooseAnswer();
-        if(answerBtn || wrongBtn) {
-            console.log("you hit a button")
-        }
-    }
+// function displayQuestion() {
+//     var questions = [q1, q2, q3, q4, q5];
+//     for(var i = 1; i < questions.length; i++){
+//         // chooseAnswer()
+//         createH2.textContent = questions[i].question;
+//         getSection[1].appendChild(createH2);
+//         createBtnWrong1.textContent = questions[i].wrongAns1;
+//         getSection[1].appendChild(createBtnWrong1);
+//         createBtnWrong2.textContent = questions[i].wrongAns2;
+//         getSection[1].appendChild(createBtnWrong2);
+//         createBtnWrong3.textContent = questions[i].wrongAns2;
+//         getSection[1].appendChild(createBtnWrong3);
+//         createBtnAnswer.textContent = questions[i].rightAns;
+//         getSection[1].appendChild(createBtnAnswer);
+//         // chooseAnswer();
+//         if(answerBtn || wrongBtn) {
+//             console.log("you hit a button")
+//         }
+//     }
     
-}
+// }
 
 
 
-function nextQuestion() {
-    var questions = [q1, q2, q3, q4, q5];
-    for(i = 1; i < questions.length; i++){
-        displayQuestion(questions[i]);
+// function nextQuestion() {
+//     var questions = [q1, q2, q3, q4, q5];
+//     for(i = 1; i < questions.length; i++){
+//         displayQuestion(questions[i]);
         
-    }
-}
+//     }
+// }
 
-startQuiz.addEventListener("click", function() {
-    startPage.setAttribute("style", "display: none;");
-    displayFirstQuestion();
-});    
+// startQuiz.addEventListener("click", function() {
+//     startPage.setAttribute("style", "display: none;");
+//     displayFirstQuestion();
+// });    
     
 
 
@@ -218,7 +221,22 @@ startQuiz.addEventListener("click", function() {
 //     getSection[1].children[0].style.display = "flex";
 // }
 
+q1 = document.getElementById("question-1");
+q2 = document.getElementById("question-2");
+q3 = document.getElementById("question-3");
+q4 = document.getElementById("question-4");
+q5 = document.getElementById("question-5");
 
+// works!
+for(i=1; i<5; i++) {
+    q2.children[i].addEventListener("click", function(event) {
+        if(event.currentTarget == document.getElementById("correct-answer")) {
+            console.log("correct answer")
+        }else{
+            console.log("wrong answer")
+        }
+    })
+}
 
 // 2. create a nav-bar with highscores on the left, timer on the right.  This can be done in html file directly
 
